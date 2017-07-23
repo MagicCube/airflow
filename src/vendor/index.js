51044f70cebdff5083c7187c7bd2aeab261fc2ff
Henry Li <henry1943@163.com>
2017-07-23 13:22:22 +0800

    Add history and react-router-redux

--- src/vendor/index.js
+++ src/vendor/index.js
@@ -1,8 +1,10 @@
 import 'babel-polyfill';
+import 'history';
 import 'prop-types';
 import 'react';
 import 'react-dom';
 import 'react-redux';
+import 'react-router-redux';
 import 'redux';
 import 'redux-actions';
 import 'redux-promise';
