f0eb2ab865f3fb8458ac648ff397a16210078093
Henry Li <henry1943@163.com>
2017-07-23 20:50:22 +0800

    Remove connect

--- src/apps/cloud-downloader/components/App/index.jsx
+++ src/apps/cloud-downloader/components/App/index.jsx
@@ -1,29 +1,11 @@
-import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import actions from '../../actions';
-import connect from '../../connect';
 import NavList from '../NavList';
 
 import './index.less';
 
-@connect(
-  state => ({ ...state }),
-  dispatch => ({ actions: bindActionCreators(actions, dispatch) })
-)
 export default class App extends Component {
-  static propTypes = {
-    actions: PropTypes.shape({
-      loadTasks: PropTypes.func
-    }).isRequired
-  }
-
-  componentDidMount() {
-    this.props.actions.loadTasks();
-  }
-
   render() {
     return (
       <div className="cd-app">
