948cca32611a4e2b32387b483453143962511b30
Henry Li <henry1943@163.com>
2017-07-23 13:22:07 +0800

    Add react-router

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,34 @@
-if (process.env.NODE_ENV === 'production') {
-  exports.configStore = require('./configStore').default;
-} else {
-  exports.configStore = require('./configStore.dev').default;
+import { routerMiddleware } from 'react-router-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise';
+
+import history from '../history';
+import reducer from './reducer';
+
+const DEV_MODE = process.env.NODE_ENV !== 'production';
+
+export function configStore() {
+  const historyRouterMiddleware = routerMiddleware(history);
+
+  let composeEnhancers = null;
+  if (DEV_MODE) {
+    composeEnhancers = (typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      : compose;
+  } else {
+    composeEnhancers = compose;
+  }
+
+  const store = createStore(
+    reducer,
+    undefined,
+    composeEnhancers(
+      applyMiddleware(
+        historyRouterMiddleware,
+        promiseMiddleware
+      )
+    )
+  );
+
+  return store;
 }
