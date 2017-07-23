f89c22ee038ac43d7f2f22c3bff69bbc21915692
Henry Li <henry1943@163.com>
2017-07-23 20:50:28 +0800

    Remove connect

--- src/workspace/index.js
+++ src/workspace/index.js
@@ -1,13 +1,6 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-import actions from './actions';
 import combinedReducer from './reducers';
 import Workspace from './components/Workspace';
 
-export default connect(
-  state => ({ ...state.workspace, router: state.router }),
-  dispatch => ({ actions: bindActionCreators(actions, dispatch) })
-)(Workspace);
+export default Workspace;
 
 export const reducer = combinedReducer;
