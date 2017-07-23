9e5bf8b371c64a35ad6e1e09d3329f2b21d70674
Henry Li <henry1943@163.com>
2017-07-23 21:23:16 +0800

    Add route and TaskList

--- src/apps/cloud-downloader/components/App/index.jsx
+++ src/apps/cloud-downloader/components/App/index.jsx
@@ -1,8 +1,10 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import meta from '../../meta';
 import NavList from '../NavList';
+import TaskList from '../TaskList';
 
 import './index.less';
 
@@ -18,9 +20,23 @@ export default class App extends Component {
             <Route exact path={meta.path}>
               <Redirect to={`${meta.path}/downloading`} />
             </Route>
+            <Route path={`${meta.path}/:filter`} component={matchParams(TaskList)} />
           </Switch>
         </main>
       </div>
     );
   }
+}
+
+function matchParams(WrappedComponent) {
+  const Hoc = ({ match }) => {
+    const params = match.params;
+    return <WrappedComponent {...params} />;
+  };
+  Hoc.propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({})
+    })
+  };
+  return Hoc;
 }
---
+++ src/apps/cloud-downloader/components/TaskList/index.jsx
@@ -0,0 +1,61 @@
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import actions from '../../actions';
+import connect from '../../connect';
+
+import './index.less';
+
+@connect(
+  state => ({ tasks: state.tasks }),
+  dispatch => ({ actions: bindActionCreators(actions, dispatch) })
+)
+export default class TaskList extends Component {
+  static propTypes = {
+    filter: PropTypes.string,
+    tasks: PropTypes.arrayOf(PropTypes.shape({
+      gid: PropTypes.string,
+    })).isRequired,
+
+    actions: PropTypes.shape({
+      loadTasks: PropTypes.func
+    }).isRequired
+  }
+
+  static defaultProps = {
+    filter: null,
+    tasks: []
+  }
+
+  componentDidMount() {
+    this.props.actions.loadTasks(this.props.filter);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.filter !== this.props.filter) {
+      this.props.actions.loadTasks(this.props.filter);
+    }
+  }
+
+
+  render() {
+    const { tasks } = this.props;
+    return (
+      <div className="cd-task-list">
+        <ul>
+          {
+            tasks.map(task => (
+              <li
+                key={task.gid}
+                id={task.gid}
+              >
+                Task
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
