5bbfa69a124272384eb49488d8ae235fd01fe05f
Henry Li <henry1943@163.com>
2017-07-23 13:20:30 +0800

    Remove index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
+import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { render } from 'react-dom';
 
+import history from './history';
 import { configStore } from './store';
 import Workspace from './workspace';
 
-import './index.html';
 import './index.less';
 
 const store = configStore();
 
 render(
   <Provider store={store}>
-    <Workspace />
+    <ConnectedRouter history={history}>
+      <Workspace />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('react-mount-point')
 );
