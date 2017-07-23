f3c14c78cf862410ffe1e20776f4b6131f76edd7
Henry Li <henry1943@163.com>
2017-07-23 17:23:16 +0800

    Add setup()

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const log = require('./lib/log');
+const services = require('./lib/services');
 
 const PRODUCTION_MODE = process.env.NODE_ENV === 'production';
 
-log.info(`Airflow is now starting in ${PRODUCTION_MODE ? 'PRODUCTION' : 'DEVELOPMENT'} mode...`);
-// Start HTTP server
-require('./lib/server/http-server');
+async function setup() {
+  log.info(`Airflow is now starting in ${PRODUCTION_MODE ? 'PRODUCTION' : 'DEVELOPMENT'} mode...`);
+
+  await services.setup();
+  await services.start();
+}
+
+setup();
