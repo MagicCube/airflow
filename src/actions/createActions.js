3025d76e2d67497376225928556b7cfde4631c4c
Henry Li <henry1943@163.com>
2017-07-23 11:48:05 +0800

    Bugfix

--- src/actions/createActions.js
+++ src/actions/createActions.js
@@ -3,7 +3,7 @@ import { createAction } from 'redux-actions';
 export default function createActions(path, actionCreators) {
   const actionNames = Object.keys(actionCreators);
   return actionNames.reduce((actions, actionName) => {
-    actions[actionName] = createAction(`path/${actionName}`, actionCreators[actionName]);
+    actions[actionName] = createAction(`${path}/${actionName}`, actionCreators[actionName]);
     return actions;
   }, {});
 }
