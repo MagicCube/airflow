f9e8e3b0e8e9438a5f576e38a6453513e07d2712
Henry Li <henry1943@163.com>
2017-07-24 14:27:28 +0800

    Color it green when completed

--- src/apps/cloud-downloader/components/ProgressBar/index.jsx
+++ src/apps/cloud-downloader/components/ProgressBar/index.jsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 
@@ -13,7 +14,7 @@ export default class ProgressBar extends PureComponent {
     const { completed, total } = this.props;
     const percentage = `${(Math.round((completed / total) * 10000) / 100)}%`;
     return (
-      <div className="cd-progress-bar">
+      <div className={cn('cd-progress-bar', { completed: (percentage === '100%') })}>
         <div className="block" style={{ width: percentage }} />
         <div className="percentage">{percentage}</div>
       </div>
