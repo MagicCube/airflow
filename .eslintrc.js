a7e99339d31fe5d30afc487c0c1d631b532c7101
Henry Li <henry1943@163.com>
2017-07-29 15:46:33 +0800

    Change rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,9 @@ module.exports = {
     "node": true
   },
   "plugins": [
-    "react",
+    "import",
     "jsx-a11y",
-    "import"
+    "react"
   ],
   rules: {
     "class-methods-use-this": "off",
@@ -16,13 +16,16 @@ module.exports = {
     "global-require": "off",
     "import/no-extraneous-dependencies": "off",
     "import/prefer-default-export": "off",
-    "jsx-a11y/href-no-hash": "off",
     "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }],
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/label-has-for": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
     "max-len": "warn",
     "no-else-return": "off",
-    "no-underscore-dangle": "off",
     "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    "react/forbid-prop-types": "warn",
     "react/prefer-stateless-function": "off",
-    "react/forbid-prop-types": "warn"
+    "react/no-unused-prop-types": "warn"
   }
 };
