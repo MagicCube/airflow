20eb9686fa830d00dc5b7ee119769c06716e3972
Henry Li <henry1943@163.com>
2017-07-29 15:47:23 +0800

    Add close button

--- src/workspace/components/Dialog/index.jsx
+++ src/workspace/components/Dialog/index.jsx
@@ -10,7 +10,8 @@ export default class Dialog extends Component {
     title: PropTypes.string,
     children: React.PropTypes.element,
     width: React.PropTypes.number,
-    height: React.PropTypes.number
+    height: React.PropTypes.number,
+    onCloseButtonClick: React.PropTypes.func
   }
 
   static defaultProps = {
@@ -18,7 +19,12 @@ export default class Dialog extends Component {
     title: null,
     children: null,
     width: 540,
-    height: 600
+    height: 600,
+    onCloseButtonClick: noop
+  }
+
+  handleCloseButtonClick = () => {
+    this.props.onCloseButtonClick();
   }
 
   render() {
@@ -28,6 +34,7 @@ export default class Dialog extends Component {
         <div className={cn('af-workspace-dialog', className)} style={{ width, height }}>
           <header>
             <h3>{title}</h3>
+            <a className="close-button" title="关闭" onClick={this.handleCloseButtonClick}><i className="fa fa-times" /></a>
           </header>
           <main>
             {children}
@@ -36,4 +43,9 @@ export default class Dialog extends Component {
       </div>
     );
   }
+}
+
+
+function noop() {
+
 }
