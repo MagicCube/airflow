39f2059d763e8caa76d81bc1560ac2c41ae4aac5
Henry Li <henry1943@163.com>
2017-07-29 15:47:01 +0800

    Add default mapStateToProps

--- src/apps/cloud-downloader/connect.js
+++ src/apps/cloud-downloader/connect.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 
 import meta from './meta';
 
-export default function (mapStateToProps, mapDispatchToProps) {
+export default function (mapStateToProps = state => state, mapDispatchToProps) {
   return connect(
     state => mapStateToProps({ ...state[meta.id], router: state.router }, state),
     mapDispatchToProps
