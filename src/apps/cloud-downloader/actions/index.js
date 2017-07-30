7c6592e28548f5541743ad9148abf446bdae5a3d
Henry Li <henry1943@163.com>
2017-07-30 12:03:42 +0800

    Add createTask()

--- src/apps/cloud-downloader/actions/index.js
+++ src/apps/cloud-downloader/actions/index.js
@@ -1,13 +1,8 @@
-import axios from 'axios';
-
 import createActions from '../../../actions/createActions';
+import * as api from '../api';
 import meta from '../meta';
 
 export default createActions(meta.id, {
-  async loadTasks(filter = 'downloading') {
-    const tasks = (await axios.get('/api/download/task', {
-      params: { filter }
-    })).data;
-    return tasks;
-  }
+  createTask: api.createTask,
+  loadTasks: api.getTasks
 });
